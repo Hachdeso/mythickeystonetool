16b096203f2788bd5852746cbaaf181774e1cd2e
= <=>
2021-01-20 20:09:38 +0100

    url prod bug fix

--- webapp/src/URL.ts
+++ webapp/src/URL.ts
@@ -1,2 +1,2 @@
 // export const serverURL: string = "http://localhost:4000";
-export const serverURL: string = "http://http://82.64.232.24:4000";
+export const serverURL: string = "http://82.64.232.24:4000";
